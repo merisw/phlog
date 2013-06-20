260754a223251f6ae80d5d758e072be937d4fd5d
Meris Williams <merisw@gmail.com>
2013-06-19 21:55:35 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phlog::Application.config.secret_token = '359aca8c24b9623aea86ecd7f88baa4a2970b9eef233def64d603f7e19e164d97965d02c0135eb0d597d39281ee365c83b3217f41e10c09558be9234ea1a3fd9'
